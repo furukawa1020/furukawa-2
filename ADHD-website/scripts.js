5f9d85b986e3eda24459772df850a58423773d7d
furukawa1020 <f.kotaro.0530@gmail.com>
2025-02-17 17:02:28 +0900

    Initial commit for ADHD support website

---
+++ ADHD-website/scripts.js
@@ -0,0 +1,21 @@
+// Placeholder for Pomodoro Timer
+function startPomodoro() {
+    console.log("Pomodoro Timer Started");
+}
+
+// Placeholder for White Noise Generator
+function playWhiteNoise() {
+    console.log("Playing White Noise");
+}
+
+// Placeholder for Simple To-Do List
+function addToDoItem() {
+    console.log("To-Do Item Added");
+}
+
+// Event listeners for buttons (to be added in HTML)
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('start-pomodoro').addEventListener('click', startPomodoro);
+    document.getElementById('play-white-noise').addEventListener('click', playWhiteNoise);
+    document.getElementById('add-todo-item').addEventListener('click', addToDoItem);
+});
